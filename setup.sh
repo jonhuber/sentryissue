666f11f57d83623e700cb9bbe3859b39d6b9bd30
Jon Huber <jon@jonhuber.com>
2025-04-29 08:58:35 -0500

    Make pyenv work inside the setup script

--- setup.sh
+++ setup.sh
@@ -23,7 +23,10 @@ curl -fsSL https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
-source ~/.bashrc
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
 
 pyenv install 3.6.15
 pyenv local 3.6.15
