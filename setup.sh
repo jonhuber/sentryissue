02618c5d1f6103e26051c13bfe0a110be0204267
Jon Huber <jon@jonhuber.com>
2025-04-29 08:36:22 -0500

    Fix pyenv setup with proper quoting

--- setup.sh
+++ setup.sh
@@ -20,9 +20,9 @@ sudo apt install --yes \
 
 curl -fsSL https://pyenv.run | bash
 
-echo "export PYENV_ROOT=\"$HOME/.pyenv\"" >> ~/.bashrc
-echo "[ -d $PYENV_ROOT/bin ] && export PATH=\"$PYENV_ROOT/bin:$PATH\"" >> ~/.bashrc
-echo "eval \"$(pyenv init - bash)\"" >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
 source ~/.bashrc
 
 pyenv install 3.6.15
